20651fa0ccc8d7299f91d762e8cbcc4caa7c2afe
Oli Zimpasser <oglimmer@gmail.com>
2022-11-10 22:34:28 +0100

    rewrite forEach with for await to run loop in a sequence instead of in parallel

--- src/action/registerCommands.ts
+++ src/action/registerCommands.ts
@@ -19,14 +19,18 @@ export default (client: Client, logger: Logger) => {
 
     client.once('ready', async () => {
         logger.logSync("INFO", "Registriere Commands")
-        client.guilds.cache.forEach((g) => {
-            logger.logSync("INFO", `Registriere Commands für Server ${g.name ?? "<Name nicht bekannt>"} #${g.id}`)
-            rest.put(
-                Routes.applicationGuildCommands(process.env.APPLICATION_ID || "", g.id),
-                { body: CommandList }
-            ).then(() => logger.logSync("INFO", `Commands für Server ${g.name ?? "<Name nicht bekannt>"} #${g.id} registriert`))
-             .catch(() => logger.logSync("WARN", `Commands konnten für Server ${g.name ?? "<Name nicht bekannt>"} #${g.id}`))
-        })
-    })
 
-}
+        for await (const [_, g] of client.guilds.cache) {
+            try {
+                logger.logSync("INFO", `Registriere Commands für Server ${g.name ?? "<Name nicht bekannt>"} #${g.id}`)
+                await rest.put(
+                    Routes.applicationGuildCommands(process.env.APPLICATION_ID || "", g.id),
+                    { body: CommandList }
+                )
+                logger.logSync("INFO", `Commands für Server ${g.name ?? "<Name nicht bekannt>"} #${g.id} registriert`)
+            } catch (err) {
+                logger.logSync("WARN", `Commands konnten für Server ${g.name ?? "<Name nicht bekannt>"} #${g.id} : ${err}`)
+            }
+          }
+        })
+    }
