f96f21b8a855445a6ab4c9f0f7415635b1ccf8a3
Filip Lukas Paidar <filiplukas.paidar@gmail.com>
2022-11-10 09:33:22 +0100

    Mit user-reports angefangen

--- src/Bot.ts
+++ src/Bot.ts
@@ -6,14 +6,36 @@ import status from "./listeners/status";
 import interactionCreate from "./listeners/interactionCreate";
 import { Logger, LogManager } from './logger/logger';
 import registerCommands from './action/registerCommands'
+import { verbose } from 'sqlite3';
+
+const sqlite = verbose()
+
 
 let logManager: LogManager = new LogManager('./logs');
-
+let db_logger = logManager.logger('sqlite3')
 
 dotenv.config()
 
 const token = process.env.BOT_TOKEN;
 
+let db = new sqlite.Database(process.env.DB_PATH ?? "./sqlite3.db",
+    (err) => {
+        if (err) {
+            db_logger.logSync("ERROR", `DB-Open failed: ${JSON.stringify(err)}`)
+            return;
+        }
+        db_logger.logSync("INFO", "DB opened.")
+        db.run(`CREATE TABLE IF NOT EXISTS reports (
+identifier INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL UNIQUE,
+uuid TEXT NOT NULL UNIQUE,
+creator_id TEXT NOT NULL,
+reported_id TEXT NOT NULL,
+category TEXT NOT NULL,
+description TEXT,
+message TEXT,
+status INT NOT NULL)`)
+
+    })
 
 
 const client = new Client({
@@ -22,7 +44,7 @@ const client = new Client({
 
 ready(client, logManager)
 status(client, logManager) // set the status to Testing and Playing as the activity
-interactionCreate(client, logManager)
+interactionCreate(client, logManager, db)
 registerCommands(client, logManager.logger('Command-Registrierung'))
 
 client.login(token);
--- src/listeners/interactionCreate.ts
+++ src/listeners/interactionCreate.ts
@@ -1,22 +1,27 @@
-import { CommandInteraction, Client, Interaction, ButtonInteraction } from "discord.js";
+import { CommandInteraction, Client, Interaction, ButtonInteraction, ContextMenuCommandInteraction, ApplicationCommandType, UserContextMenuCommandInteraction } from "discord.js";
 import LogManager from "src/logger/logger";
 import { codeblocks, metafrage, about } from "../action/infoMessages";
 import { createRoleInterface } from '../action/roles_buttons_create'
 import { toggleRoles } from "../action/toggleRole";
+import startUserReport from "../action/userReport"
+import { Database } from 'sqlite3'
 
-export default (client: Client, logger: LogManager): void => {
+export default (client: Client, logger: LogManager, db: Database): void => {
     client.on("interactionCreate", async (interaction: Interaction) => {
         if (interaction.isCommand()) {
             await handleSlashCommand(client, interaction, logger);
         }
         if (interaction.isButton()) {
-            handleButtonCommand(client, interaction, logger)
+            handleButtonInteraction(client, interaction, logger)
+        }
+        if (interaction.isContextMenuCommand() && interaction.commandType == ApplicationCommandType.User) {
+            handleUserContextMenuCommand(client, interaction, logger, db)
         }
     });
 };
 
 const handleSlashCommand = async (client: Client, interaction: CommandInteraction, logger: LogManager) => {
-    // handle slash command here   
+    // handle slash command here
     switch (interaction.commandName) {
         case 'metafrage':
             metafrage(client, interaction, logger.logger('Metafrage'))
@@ -38,8 +43,14 @@ const handleSlashCommand = async (client: Client, interaction: CommandInteractio
 
 };
 
-const handleButtonCommand = async (client:Client, interaction: ButtonInteraction, logger:LogManager) => {
+const handleButtonInteraction = async (client: Client, interaction: ButtonInteraction, logger: LogManager) => {
     if (/(addRole-|removeRole-)([0-9]+)/.test(interaction.customId)) {
         toggleRoles(client, interaction, logger.logger("Toggle-Roles"))
+    }
+}
+
+const handleUserContextMenuCommand = async (client: Client, interaction: UserContextMenuCommandInteraction, logger: LogManager, db: Database) => {
+    if (interaction.commandType == ApplicationCommandType.User && interaction.commandName == "REPORT") {
+        startUserReport(interaction, client, db, logger.logger("Report-System"))
     }
 }
