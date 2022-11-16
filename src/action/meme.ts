f6aa03c9daf8e3b32f1dc702b72302e2e9244d01
Filip Lukas Paidar <filiplukas.paidar@gmail.com>
2022-11-16 06:31:55 +0100

    Added meme author to /meme

--- src/action/meme.ts
+++ src/action/meme.ts
@@ -17,9 +17,14 @@ export async function meme(client: Client, interaction: CommandInteraction, logg
     ]
     
     let pickSubReddit = subReddit[Math.floor(Math.random() * subReddit.length)]
+    
+    interaction.deferReply()
     let {data }  = await axios.get(`https://meme-api.herokuapp.com/gimme/${pickSubReddit}`);
 
     let postMeme = new EmbedBuilder()
+    .setAuthor({
+        name: data.author,
+    })
     .setTitle(data.title)
     .setImage(data.url)
     .setURL(data.postLink)
