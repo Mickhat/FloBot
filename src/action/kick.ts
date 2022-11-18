14f78facd91fcb40898ef3742019cfd1eff3acb6
Mickhat <root@mickhat.xyz>
2022-11-18 10:57:49 +0100

    removed sleep function; small code improvements

--- src/action/kick.ts
+++ src/action/kick.ts
@@ -20,15 +20,13 @@ export default async (client: Client, interaction: CommandInteraction, logger: L
     .setAuthor({name: `Gekickt von: ${interaction.user.tag}`,})
     .setTimestamp()
     
-    const sleep = (ms: number | undefined) => new Promise(r => setTimeout(r, ms));
-    client.users.fetch(target).then((dm =>
-    dm.send(`Du wurdest von Florian Dalwigk's Server gekickt.\nGrund: ${reason}`))).catch(e => logger.logSync("ERROR", e))
-    await sleep(250)
     try {
+        const dm = await client.users.fetch(target)
+        await dm.send(`Du wurdest von Florian Dalwigk's Server gekickt.\nGrund: ${reason}`)    
         
         await interaction.guild?.members.kick(target, reason)
 
-        interaction.reply({ embeds: [kickEmbed] })
+        await interaction.reply({ embeds: [kickEmbed] })
         logger.logSync("Info", "Kick wurde erfolgreich ausgefuehrt")
         logger.logSync("Info", `User <@${target.toString()}> wurde gekickt.Grund: ${reason}`)
     } catch (err) {
