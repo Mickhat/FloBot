1d5ca97083c559690b11510ecc93860602b81629
Mickhat <root@mickhat.xyz>
2022-11-22 18:25:36 +0100

    added /timeout command

---
+++ src/action/timeout.ts
@@ -0,0 +1,34 @@
+import { Client, CommandInteraction, EmbedBuilder, escapeMarkdown, GuildMember } from 'discord.js'
+import { Logger } from '../logger/logger'
+import ms from 'ms';
+
+
+export default async(client: Client, interaction: CommandInteraction, logger: Logger) => {
+
+    if (!interaction.isRepliable() || !interaction.reply) {
+        logger.logSync("ERROR", "Gegebene interaction kann nicht beantwortet werden.")
+        return;
+    }
+
+    const reason = escapeMarkdown(interaction.options.get('reason',true).value?.toString() || "");
+    const member = interaction.options.getMember('target') as GuildMember
+    const zeit = ms(interaction.options.get('zeit')?.value as string)
+
+
+    const embed = new EmbedBuilder()
+        .setTitle("User hat einen Timeout bekommen")
+        .setDescription(`<@${member.id}> hat einen Timeout bekommen.\nAngegebener Grund:  ${reason}`)
+        .setColor('Yellow')
+        .setAuthor({name: `Timeout durch ${interaction.user.tag}`,})
+        .setTimestamp()
+        
+    try {
+        await member?.timeout(zeit)
+
+        await interaction.reply({ embeds: [embed] })
+    } catch (err) {
+        logger.logSync("ERROR", `Timeout konnte nicht ausgefuehrt werden. ${err}`)
+
+    }
+
+}
