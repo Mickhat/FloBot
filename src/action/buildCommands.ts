a2f3a65bbba1a5ab38c3b8820ebc285e16a0fc15
Barsch2006 <cchr98756@gmail.com>
2022-11-23 18:12:32 +0100

    comment mickhat fixed

--- src/action/buildCommands.ts
+++ src/action/buildCommands.ts
@@ -124,7 +124,7 @@ export default [
     .setDescription('Entfernt eine Person von der Blacklist')
     .addUserOption(
       opt => opt.setName('target')
-        .setDescription('Die Person, die unbannt werden soll')
+        .setDescription('Die Person, die entbannt werden soll')
         .setRequired(true)
     ),
   /*
