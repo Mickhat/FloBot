7c2ae96dc2e92e8a6f2f4fdb26e4e175ddfc8a7e
Oli Zimpasser <oglimmer@gmail.com>
2022-12-03 15:26:56 +0100

    Fix crash if /meme cannot access the meme server

--- src/action/meme.ts
+++ src/action/meme.ts
@@ -1,6 +1,6 @@
 import { ActionRowBuilder, ButtonBuilder, ButtonStyle, Client, CommandInteraction, EmbedBuilder, Colors } from 'discord.js'
 import { ILogger } from 'src/logger/logger'
-import axios from 'axios'
+import axios, { AxiosError } from 'axios'
 
 export async function meme (client: Client, interaction: CommandInteraction, logger: ILogger): Promise<void> {
   if (!interaction.isRepliable()) {
@@ -16,20 +16,20 @@ export async function meme (client: Client, interaction: CommandInteraction, log
 
   const pickSubReddit = subReddit[Math.floor(Math.random() * subReddit.length)]
 
-  const { data } = await axios.get(`https://meme-api.herokuapp.com/gimme/${pickSubReddit}`)
-
-  const postMeme = new EmbedBuilder()
-    .setAuthor({
-      name: `/u/${data.author as string}`
-    })
-    .setTitle(data.title)
-    .setImage(data.url)
-    .setURL(data.postLink)
-    .setColor(Colors.Green)
-    .setTimestamp()
-    .setFooter({ text: `/r/${data.subreddit as string}  • Upvotes: ${data.ups as string} ` })
-
   try {
+    const { data } = await axios.get(`https://meme-api.herokuapp.com/gimme/${pickSubReddit}`)
+
+    const postMeme = new EmbedBuilder()
+      .setAuthor({
+        name: `/u/${data.author as string}`
+      })
+      .setTitle(data.title)
+      .setImage(data.url)
+      .setURL(data.postLink)
+      .setColor(Colors.Green)
+      .setTimestamp()
+      .setFooter({ text: `/r/${data.subreddit as string}  • Upvotes: ${data.ups as string} ` })
+
     await interaction.reply({
       embeds: [postMeme],
       components: [
@@ -49,6 +49,31 @@ export async function meme (client: Client, interaction: CommandInteraction, log
       logger.logSync('INFO', 'Deleted delete-button')
     }, 30000)
   } catch (err) {
-    logger.logSync('ERROR', `Meme konnte nicht gesendet werden. ${JSON.stringify(err)}`)
+    if (err instanceof AxiosError) {
+      let errorText
+      if (err.response) {
+        errorText = `http code was ${err.response.status} with ${JSON.stringify(err.response.data)}`
+      } else if (err.request) {
+        errorText = `No response. ${JSON.stringify(err.request)}`
+      } else {
+        errorText = `Error using axsio. ${JSON.stringify(err)}`
+      }
+      logger.logSync('ERROR', `Meme konnte nicht gesendet werden. ${errorText}`)
+    } else {
+      logger.logSync('ERROR', `Meme konnte nicht gesendet werden. ${JSON.stringify(err)}`)
+    }
+    const postMeme = new EmbedBuilder()
+      .setAuthor({
+        name: `Senior Error-Helper`
+      })
+      .setTitle('Es ist ein katastrophler Fehler beim laden des MEMEs aufgetreten! Es besteht jedoch kein Grund zur Panik! Bleiben sie ruhig!!!')
+      .setImage('https://memesfeel.com/wp-content/uploads/2022/05/Everything-Is-Fine-Meme-1.jpg')
+      .setColor(Colors.Green)
+      .setTimestamp()
+    await interaction.reply({
+      embeds: [postMeme],
+      components: [],
+      ephemeral: false
+    })
   }
 }
