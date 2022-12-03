6c4cc518fc1c712ba9083a8ec53a47e2bd05a9d4
Barsch2006 <cchr98756@gmail.com>
2022-12-03 15:53:52 +0100

    /history finished

--- src/action/history.ts
+++ src/action/history.ts
@@ -10,7 +10,11 @@ export default async function (client: Client, interaction: CommandInteraction, 
     const resultkick = await db.allAsync(`SELECT * FROM records WHERE dc_id = ? AND type = ?`, [target.id, 'KICK'])
     const resultstrike = await db.allAsync(`SELECT * FROM records WHERE dc_id = ? AND type = ?`, [target.id, 'STRIKE'])
     const resultwarn = await db.allAsync(`SELECT * FROM records WHERE dc_id = ? AND type = ?`, [target.id, 'WARN'])
-
+    const result = await db.allAsync(`SELECT points FROM records WHERE dc_id = ?`, [target.id])
+    let gespoints = 0
+    result.forEach((row) => {
+      gespoints = gespoints + parseInt(row?.points)
+    })
     const dataEmbed = new EmbedBuilder()
       .setTitle(`${target.displayName}`)
       .setThumbnail(`${target.displayAvatarURL()}`)
@@ -21,34 +25,40 @@ export default async function (client: Client, interaction: CommandInteraction, 
         Anzahl Kicks: ${resultkick.length}
         Anzahl Strikes: ${resultstrike.length}
         Anzahl Warns: ${resultwarn.length}
+        Anzahl Points: ${gespoints}
         `
       )
       .setColor(Colors.Yellow)
       .setTimestamp()
-    resultban.forEach(row => {
-      dataEmbed.addFields(
-        // eslint-disable-next-line @typescript-eslint/restrict-template-expressions
-        { name: `Reason for ${row?.type} Weight: ${row?.points}`, value: `\b ${row?.reason}` }
-      )
-    })
+    dataEmbed.addFields({ name: '\u200b', value: '__Kicks__' })
     resultkick.forEach(row => {
       dataEmbed.addFields(
         // eslint-disable-next-line @typescript-eslint/restrict-template-expressions
-        { name: `Reason for ${row?.type} Weight: ${row?.points}`, value: `\b ${row?.reason}` }
+        { name: `Grund:`, value: `\b ${row?.reason}` }
       )
     })
+    dataEmbed.addFields({ name: '\u200b', value: '__Strikes__' })
     resultstrike.forEach(row => {
       dataEmbed.addFields(
         // eslint-disable-next-line @typescript-eslint/restrict-template-expressions
-        { name: `Reason for ${row?.type} Weight: ${row?.points}`, value: `\b ${row?.reason}` }
+        { name: `Grund:`, value: `\b ${row?.reason}` }
       )
     })
+    dataEmbed.addFields({ name: '\u200b', value: '__Warns__' })
     resultwarn.forEach(row => {
       dataEmbed.addFields(
         // eslint-disable-next-line @typescript-eslint/restrict-template-expressions
-        { name: `Reason for ${row?.type}`, value: `\b ${row?.reason}` }
+        { name: `Grund:`, value: `\b ${row?.reason}` }
       )
     })
+    // Banns
+    /* dataEmbed.addFields({ name: '\u200b', value: '__Banns__' })
+    resultban.forEach(row => {
+      dataEmbed.addFields(
+        // eslint-disable-next-line @typescript-eslint/restrict-template-expressions
+        { name: `Grund:`, value: `\b ${row?.reason}` }
+      )
+    }) */
     logger.logSync("INFO", `History from ${target.id}`)
     await interaction.reply({
       embeds: [
