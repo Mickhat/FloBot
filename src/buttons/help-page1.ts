f27f718f14cd4f56e4bd0315719cd13c31f1854e
heeecker <89743412+filip326@users.noreply.github.com>
2023-08-26 10:11:12 +0200

    changed help command + buttons

---
+++ src/buttons/help-page1.ts
@@ -0,0 +1,31 @@
+import { ActionRowBuilder, ButtonBuilder, ButtonInteraction, ButtonStyle, EmbedBuilder } from "discord.js"
+
+export default {
+  buttonId: /help-page1/,
+  async execute (interaction: ButtonInteraction): Promise<void> {
+    await interaction.update({
+      embeds: [
+        new EmbedBuilder().setTitle('Hilfe-Seite')
+          .setDescription('Hier ist die Hilfe-Seite.\n\n__**Inhalt**__')
+          .addFields(
+            { name: 'Informationen', value: 'Nachrichten, in denen Nutzer Informationen erhalten - Seite 2', inline: false },
+            { name: 'Nutzer reporten', value: 'Nutzer, die gegen die Regeln dieses Servers verstoßen melden - Seite 3', inline: false },
+            { name: 'Umfragen', value: 'Umfragen auf diesem Server - Seite 4', inline: false }
+          )
+          .setFooter({
+            text: 'Seite 1/4'
+          })
+      ],
+      components: [
+        new ActionRowBuilder<ButtonBuilder>()
+          .addComponents(
+            new ButtonBuilder()
+              .setLabel('Nächste Seite')
+              .setEmoji('▶️')
+              .setStyle(ButtonStyle.Primary)
+              .setCustomId('help-page2')
+          )
+      ]
+    })
+  }
+}
---
+++ src/commands/help.ts
@@ -0,0 +1,33 @@
+import { ActionRowBuilder, ButtonBuilder, ButtonStyle, CommandInteraction, EmbedBuilder, SlashCommandBuilder } from "discord.js"
+
+export default {
+  data: new SlashCommandBuilder().setName('help')
+    .setDescription('Was kann ich?'),
+  async execute (interaction: CommandInteraction): Promise<void> {
+    await interaction.reply({
+      embeds: [
+        new EmbedBuilder().setTitle('Hilfe-Seite')
+          .setDescription('Hier ist die Hilfe-Seite.\n\n__**Inhalt**__')
+          .addFields(
+            { name: 'Informationen', value: 'Nachrichten, in denen Nutzer Informationen erhalten - Seite 2', inline: false },
+            { name: 'Nutzer reporten', value: 'Nutzer, die gegen die Regeln dieses Servers verstoßen melden - Seite 3', inline: false },
+            { name: 'Umfragen', value: 'Umfragen auf diesem Server - Seite 4', inline: false }
+          )
+          .setFooter({
+            text: 'Seite 1/4'
+          })
+      ],
+      components: [
+        new ActionRowBuilder<ButtonBuilder>()
+          .addComponents(
+            new ButtonBuilder()
+              .setLabel('Nächste Seite')
+              .setEmoji('▶️')
+              .setStyle(ButtonStyle.Primary)
+              .setCustomId('help-page2')
+          )
+      ],
+      ephemeral: true
+    })
+  }
+}
